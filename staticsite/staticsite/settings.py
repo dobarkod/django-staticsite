2fe2c2edcc420c07975e4f020ab83ba084e0b5df
Senko Rasic <senko.rasic@dobarkod.hr>
2014-02-15 23:17:15 +0100

    initial commit

---
+++ staticsite/staticsite/settings.py
@@ -0,0 +1,21 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_KEY = 'izpxs7y)yvl0dov6!nnm_5_cy4%!f38zpayn2zv3b1gtmqk0!+'
+DEBUG = TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = ['*']
+INSTALLED_APPS = (
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+ROOT_URLCONF = 'staticsite.urls'
+WSGI_APPLICATION = 'staticsite.wsgi.application'
+STATIC_URL = '/static/'
---
+++ staticsite/staticsite/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'staticsite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+)
