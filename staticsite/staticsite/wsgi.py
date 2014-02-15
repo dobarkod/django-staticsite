3d53beb948f5bbae14b608fde0bec53b7370e3c1
Senko Rasic <senko.rasic@dobarkod.hr>
2014-02-15 23:55:32 +0100

    use dj-static for serving static files

--- staticsite/staticsite/wsgi.py
+++ staticsite/staticsite/wsgi.py
@@ -1,14 +1,7 @@
-"""
-WSGI config for staticsite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staticsite.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
