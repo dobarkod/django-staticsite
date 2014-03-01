8cedc2519e4cbd9b27cf5a8b74384a9dfb054bb0
gilsondev <contato@gilsondev.com>
2014-03-01 18:46:36 -0300

    Create google analytics context processor

---
+++ staticsite/pages/context_processors.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.template.loader import render_to_string
+
+
+def google_analytics(self):
+    """Context processor
+    for render analytics code.
+
+    Insert in template:
+
+        {{ analytics_code }}
+    """
+    if settings.DEBUG is not False:
+        return {
+            'analytics_code': render_to_string('analytics.html', {
+                'ANALYTICS_CODE': settings.GOOGLE_ANALYTICS_KEY,
+                'ANALYTICS_DOMAIN': settings.GOOGLE_ANALYTICS_DOMAIN
+            })
+        }
+    else:
+        return {'analytics_code': ""}
